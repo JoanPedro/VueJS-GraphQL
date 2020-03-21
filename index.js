5392ce0c4eb58c5b92201159449101269ed70035
Joan Pedro <jpos.senai@gmail.com>
2020-03-21 19:29:35 -0300

    Adiciona Consulta com Objetos

--- index.js
+++ index.js
@@ -4,10 +4,26 @@ const typeDefs = gql`
     # Pontos de Entrada da API
     scalar Date
 
+    type Atributo {
+        forca: Float!
+        agilidade: Float!
+        inteligencia: Float!
+        vitalidade: Float!
+    }
+
+    type Personagem {
+        id: ID!
+        nome: String!
+        classe: String!
+        atributos: Atributo!
+        master: Boolean
+    }
+
     type Query {
-        ola: String
-        horaAtual: String
-        formaScalar: Date
+        ola: String!
+        horaAtual: String!
+        formaScalar: Date!
+        criarPersonagem: Personagem
     }
 `
 
@@ -24,7 +40,22 @@ const resolvers = {
         },
         formaScalar() {
             return new Date()
+        },
+        criarPersonagem(){
+            return {
+                id: 1,
+                nome: 'Lancelote',
+                classe: 'Ferreiro',
+                atributos: {
+                    forca: 15,
+                    agilidade: 9,
+                    inteligencia: 4,
+                    vitalidade: 10
+                },
+                master: true                
+            }
         }
+
     }
 }
 
